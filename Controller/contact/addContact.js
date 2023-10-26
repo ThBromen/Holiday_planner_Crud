29a0c7bec54c306f4833c37820bf3ffcf49abf16
ThBromen <hashimwimanatheogene34@gmail.com>
2023-10-26 12:20:21 +0200

    sending email to contact

--- Controller/contact/addContact.js
+++ Controller/contact/addContact.js
@@ -1,11 +1,29 @@
-import { contact } from "../../models";
+import { contact,User } from "../../models";
+import { transporter } from "../../utils";
 
 
 export const addContact = async (req, res) => {
 try{
-  await contact.create(req.body);
-      console.log("contact is created successfully");
+      const emailExistsInUsers = await User.exists({ email: req.body.email });
+
+      const mailOptions = {
+        from: process.env.EMAIL_USER,
+        to: req.body.email,
+        subject: 'Thank you for your feedback',
+        text: `Hello ${User.fullNames} !!     Thank you for your feedback on our tour.           We are excited to have you!`,
+      };
       
+      transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+          console.error('Error sending email:', error);
+        } else {
+          console.log('Email sent:', info);
+        }
+      });
+
+
+      await contact.create(req.body);
+      console.log("contact is created successfully");
   res.status(201).json({
     message: "contact is created successfully",
   });
